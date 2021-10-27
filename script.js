bcb552b4ea92dcce74c7376c99a41887e12cc0ce
Pedro Matos <pedro_matos2012@hotmail.com>
2021-10-27 17:01:32 -0400

    continua...

---
+++ script.js
@@ -0,0 +1,45 @@
+// let produz uma variavel, pode ser redefinida.
+let idade = 23;
+let altura = 175;
+let peso = 60;
+let nome = 'Pedro';
+altura = 179;
+peso = 59
+
+// const produz uma variavel, não pode ser redefinida.
+const cor = 'branco'; 
+const sexo = 'masculino';
+
+// var produz uma variavel
+var namorada = undefined;
+var programador = true;
+var emprego = null;
+
+console.log(idade, altura, peso, nome, cor, sexo, namorada, programador, emprego);
+
+// objects são variaveis de um mesmo objeto juntas
+let pessoa = {
+    nome: 'Roberto',
+    idade: 38,
+    altura: 190,
+    peso: 82,
+    emprego: undefined
+}
+
+// arrays
+let família = ['Natália', 12, 166, 47]
+let amigos = ['Gabriel', 'Kevelin', 'Lucas', 'Karem', 'Lidânia']
+
+console.log(pessoa, família, amigos[3]);
+
+
+
+
+
+
+
+//window.alert("Alô, isso é apenas um teste de alerta do site, onde tento criar uma tela de alerta para que, quem entrar possa ler o alerta de introdução da página, assim perdendo um temp absurdamente gigante da vida. Obrigad por ler!")
+
+//window.confirm("Alô !!!! este é diferenciado, você precisa confirmar que leu ou cencelar que leu, eu acho.")
+
+//window.prompt("Sério kkkkk esse é o último, só vai aí. Coloca seu nome, apelido ou qualquer coisa.")
